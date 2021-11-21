8ff8b6105448fb85e7ac586ae06914d2387d715c
mithun9421 <mithunmuralee94@gmail.com>
2021-11-21 22:51:42 +0530

    Added invalidation step to build

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,6 +1,6 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
-console.log('iteration 3');
+console.log('iteration 4');
 export default () => {
 	const ref = useRef(null);
 	useEffect(() => {

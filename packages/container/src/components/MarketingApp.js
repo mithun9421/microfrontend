e565b97003a39a24f290cc1bcdc621d3276e1fee
mithun9421 <mithunmuralee94@gmail.com>
2021-11-21 21:06:56 +0530

    Update MarketingApp.js

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,5 +1,6 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+console.log('iteration 1');
 export default () => {
 	const ref = useRef(null);
 	useEffect(() => {

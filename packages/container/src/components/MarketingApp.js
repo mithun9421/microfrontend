865242ab8e15e9a346fdf1e566fc38c769502cf2
mithun9421 <mithunmuralee94@gmail.com>
2021-11-21 21:23:13 +0530

    iteration 2

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,6 +1,6 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
-console.log('iteration 1');
+console.log('iteration 2');
 export default () => {
 	const ref = useRef(null);
 	useEffect(() => {

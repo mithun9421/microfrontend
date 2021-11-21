967d026cbf910b1aa0e47b2f23dd9aa30cef17c2
mithun9421 <mithunmuralee94@gmail.com>
2021-11-21 23:34:30 +0530

    Added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
-import { mount } from 'marketing/MarketingApp';
-
-console.log(mount);
+import Header from './components/Header';
 
 export default () => {
 	return (
-		<div>
-			<h1>Hi there, from container, checking github actions as well!</h1>
-			<hr />
-			<MarketingApp />
-		</div>
+		<BrowserRouter>
+			<div>
+				<Header />
+				<h1>Hi there, from container, checking github actions as well!</h1>
+				<hr />
+				<MarketingApp />
+			</div>
+		</BrowserRouter>
 	);
 };

f2d5986bd0a04a54bb0caeed11e28cc133b5bf55
mithun9421 <mithunmuralee94@gmail.com>
2021-11-21 23:51:01 +0530

    CSS Fix

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,16 +2,23 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const generateclassName = createGenerateClassName({
+	productionPrefix: 'co'
+});
 
 export default () => {
 	return (
 		<BrowserRouter>
-			<div>
-				<Header />
-				<h1>Hi there, from container, checking github actions as well!</h1>
-				<hr />
-				<MarketingApp />
-			</div>
+			<StylesProvider generateClassName={generateclassName}>
+				<div>
+					<Header />
+					<h1>Hi there, from container, checking github actions as well!</h1>
+					<hr />
+					<MarketingApp />
+				</div>
+			</StylesProvider>
 		</BrowserRouter>
 	);
 };
